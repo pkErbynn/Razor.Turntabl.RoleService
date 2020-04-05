d8d9a9f9a5c0858bb2559fa5d2e5010e3ff9ddd7
erbynn <john.erbynn@gmail.com>
2020-04-05 16:43:32 +0100

    wip: create a new emp view model

--- Turntabl.RoleService/ViewModels/NewEmployeeViewModel.cs
+++ Turntabl.RoleService/ViewModels/NewEmployeeViewModel.cs
@@ -4,23 +4,21 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace Turntabl.RoleService.Models
+namespace Turntabl.RoleService.ViewModels
 {
-    public class Employee
+    public class NewEmployeeViewModel
     {
-        public int Id { get; set; }
-
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
 
         public string Email { get; set; }
 
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
 
+        [Display(Name = "Postal Address")]
         public string Address { get; set; }
-
-
 
     }
 }
